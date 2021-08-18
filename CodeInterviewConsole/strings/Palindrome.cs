dbdd3d2a8caf2ca7fef7494b945e0e67b8b7b175
Francesc Pujol <fpbosch@gmail.com>
2021-08-18 11:29:15 +0200

    Implemented palindrome algorithm

---
+++ CodeInterviewConsole/strings/Palindrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeInterviewConsole.strings
+{
+    public class Palindrome
+    {
+
+        /// <summary>
+        /// This method should reverse the string
+        /// ex:
+        ///     input: madam, output: Palindrome
+        ///     input: step on no pets, output: Palindrome
+        ///     input: book, output: Not Palindrome
+        ///     input: 1221, output: Palindrome
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <returns></returns>
+        public string Run(string origin)
+        {
+
+            bool arePalindrome = true;
+            for (int i = 0, j=origin.Length-1; i < j; i++, j--)
+            {
+                if (origin[i] != origin[j])
+                {
+                    arePalindrome = false;
+                    break;
+                }
+            }
+
+            return (arePalindrome) ? "Palindrome" : "Not Palindrome";
+
+        }
+    }
+}
---
+++ CodeInterviewTests/PalindromeTest.cs
@@ -0,0 +1,30 @@
+﻿using CodeInterviewConsole.strings;
+using System;
+using Xunit;
+
+namespace CodeInterviewTests
+{
+
+    public class PalindromeTest
+    {
+        /// <summary>
+        /// This should test the reverse string
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="source"></param>
+        [Theory]
+        [InlineData("madam", "Palindrome")]
+        [InlineData("step on no pets", "Palindrome")]
+        [InlineData("book", "Not Palindrome")]
+        [InlineData("1221", "Palindrome")]
+        [InlineData("dad", "Palindrome")]
+        public void Should_A_String_Be_Palindrome_Or_Not(string origin, string source)
+        {
+            Palindrome sut = new Palindrome();
+            var result = sut.Run(origin);
+            Assert.Equal(result, source);
+        }
+
+    }
+
+}

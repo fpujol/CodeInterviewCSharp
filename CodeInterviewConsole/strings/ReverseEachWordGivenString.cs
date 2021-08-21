b9b953910d955ed73f9e2ae2390aacb0c4040dfd
Francesc Pujol <fpbosch@gmail.com>
2021-08-21 21:19:18 +0200

    Implemented reverse each word given string

---
+++ CodeInterviewConsole/strings/ReverseEachWordGivenString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeInterviewConsole.strings
+{
+    public class ReverseEachWordGivenString
+    {
+
+        /// <summary>
+        /// This method should reverse each word given string
+        /// ex:
+        ///     input: Welcome to Csharp corner, output: emocleW ot prahsC renroc
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <returns></returns>
+        public string Run(string str)
+        {
+            StringBuilder output = new StringBuilder();
+            List<char> charlist = new List<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ' || i == str.Length - 1)
+                {
+                    if (i == str.Length - 1)
+                        charlist.Add(str[i]);
+                    for (int j = charlist.Count - 1; j >= 0; j--)
+                        output.Append(charlist[j]);
+                    
+                    if (i != str.Length - 1)
+                        output.Append(' ');
+                    charlist = new List<char>();
+                }
+                else
+                    charlist.Add(str[i]);
+            }
+
+            return output.ToString();
+        }
+    }
+}
---
+++ CodeInterviewTests/ReverseEachWordGivenStringTest.cs
@@ -0,0 +1,26 @@
+﻿using CodeInterviewConsole.strings;
+using System;
+using Xunit;
+
+namespace CodeInterviewTests
+{
+
+    public class ReverseEachWordGivenStrinsgTest
+    {
+        /// <summary>
+        /// This should test the reverse string
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="expected"></param>
+        [Theory]
+        [InlineData("Welcome to Csharp corner", "emocleW ot prahsC renroc")]
+        public void Should_Each_Word_Be_Reversed(string origin, string expected)
+        {
+            ReverseEachWordGivenString sut = new ReverseEachWordGivenString();
+            var result = sut.Run(origin);
+            Assert.Equal(expected, result);
+        }
+
+    }
+
+}
